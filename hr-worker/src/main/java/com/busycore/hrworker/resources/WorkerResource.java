b13650b10004b13afb65598f21c1c20ffc53bba5
Matheus Vargem <matheusvrgm@gmail.com>
2021-02-16 22:14:07 -0300

    Actuator and configs

--- hr-worker/src/main/java/com/busycore/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/busycore/hrworker/resources/WorkerResource.java
@@ -5,6 +5,8 @@ import com.busycore.hrworker.repositories.WorkerRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,11 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RefreshScope
 @RestController
 @RequestMapping("/workers")
 public class WorkerResource {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
+    @Value("${test.config}")
+    private String testConfig;
 
     @Autowired
     private Environment env;
@@ -30,6 +36,12 @@ public class WorkerResource {
     public ResponseEntity<List<Worker>> findAll(){
         List<Worker> list = workerRepository.findAll();
         return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/configs")
+    public ResponseEntity<Void> getConfig(){
+        logger.info("CONFIG = "+testConfig);
+        return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/{id}")
